1b5d39fcab6c76460f772ff3ba735d5c62978d9e
dyt4516 <91649182+dyt4516@users.noreply.github.com>
2021-11-07 15:25:49 -0700

    Add changes to result showing part

--- Stock_FundamentalAnalysis.py
+++ Stock_FundamentalAnalysis.py
@@ -19,9 +19,13 @@ def fmtTime(timeStamp):
 api_key = '1e22a0373e96f71ca2343fa8ec7685fa'
 ticker_list = ['TSLA','AAPL','AMZN','GOOG','BABA','TCEHY','FB','PFE','MSFT','JD','NFLX','SBUX','CSCO','INTC','AMD','NVDA']
 
-NasDaq = {'2020':0.1742,'2019':0.1286,'2018':0.1811,'2017':0.1847,'2016':0.1379}
+NasDaq = {'2020':0.1742,'2019':0.1286,'2018':0.1811}
 
-
+def mean(lst):
+	sum=0
+	for each in lst:
+		sum+=each
+	return sum/len(lst)
 
 def data_collecting(input_file,Text):
 	ticker_list=[]
@@ -36,7 +40,7 @@ def data_collecting(input_file,Text):
 	for company in ticker_list:
 		financial_ratios_annually = fa.financial_ratios(company, api_key, period="annual")
 		ticker_data[company] = []
-		for year in ['2020','2019','2018','2017','2016']:
+		for year in ['2020','2019','2018']:
 			result = financial_ratios_annually.loc['netProfitMargin'][year]
 			ticker_data[company].append(result)
 
@@ -45,9 +49,13 @@ def data_collecting(input_file,Text):
 	for each_company in ticker_list:
 		if(win_environment(list(NasDaq.values()),ticker_data[each_company])):
 			chosen.append(each_company)
-	Text.insert(4.0,fmtTime(time.time())+'Choosen: '+str(chosen)+"\n")
+	Text.insert(4.0,fmtTime(time.time())+' Choosen: '+str(chosen)+"\n")
+	profit_2021=[]
 	for each_company in chosen:
-		break
-		financial_ratios_annually = fa.financial_ratios(each_company, api_key, period="annual")
-		profit_2021 = financial_ratios_annually.loc['netProfitMargin'][2021]
-		Text.insert(5.0,each_company+"'s 2021 profit: "+ profit_2021+" vs "+"11.9% on average"+"\n")
+		begin_data_detailed  = fa.stock_data_detailed(each_company, api_key, begin="2020-12-31", end="2021-01-01")
+		begin = begin_data_detailed.loc['2020-12-31']['close']
+		end_data_detailed  = fa.stock_data_detailed(each_company, api_key, begin="2021-11-01", end="2021-11-02")
+		end = end_data_detailed.loc['2021-11-01']['close']
+		profit_2021.append((end-begin)/begin)
+	Text.insert(5.0, "My portfolio profit is: " + str(round(mean(profit_2021), 3)) + ", compared with NasDaq average of 0.211\n" )
+
